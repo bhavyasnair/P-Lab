8352d719f7dcfbc213c349fa639661c73cd6bf2f
bhavyasnair <93581085+bhavyasnair@users.noreply.github.com>
2022-01-19 21:13:04 +0530

    Add files via upload

---
+++ longestword.py
@@ -0,0 +1,14 @@
+def words(a):
+    max1=len(a[0])
+    temp=a[0]
+    for i in a:
+        if(len(i)>max1):
+            max1=len[i]
+            temp=i
+    print("The longest word count is:",max1)
+n=input("Enter ist of words")
+a=list(n.split(','))
+print(a)
+words(a)
+    
+                     
